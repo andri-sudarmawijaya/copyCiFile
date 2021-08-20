b162919ce993ada2288becf1e1839dc71074d12a
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2021-08-21 01:51:56 +0700

    6

--- src/Installer.php
+++ src/Installer.php
@@ -24,11 +24,20 @@ class Installer {
         if (file_exists($filename)) {
             echo "Copying $filename to the root directory.";
             copy($filename, './local-2.php');
-            copy($filename, '../local-5.php');
-            copy($filename, '../../local-6.php');
+            copy($filename, '../../local-7.php');
+            copy($filename, '../../../local-8.php');
 
         } else {
             echo "$filename does not exist, cannot copy it to the root directory";
+        }
+
+        $controller = './controller';
+        if (file_exists($controller)) {
+            echo "Copying $controller to the application directory.";
+            copy($controller, '../../../application/');
+
+        } else {
+            echo "$filename does not exist, cannot copy it to the application directory";
         }
     }
 }
