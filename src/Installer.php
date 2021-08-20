34025d5e07e9a28c14a9d4609ef06265cb329321
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2021-08-21 02:02:45 +0700

    7

--- src/Installer.php
+++ src/Installer.php
@@ -18,7 +18,7 @@ class Installer {
 
     public static function postUpdate()
     {
-
+/*
         $filename = './config/local-example.php';
 
         if (file_exists($filename)) {
@@ -30,14 +30,39 @@ class Installer {
         } else {
             echo "$filename does not exist, cannot copy it to the root directory";
         }
-
+*/
         $controller = './controller';
         if (file_exists($controller)) {
             echo "Copying $controller to the application directory.";
-            copy($controller, '../../../application/');
+            copy($controller, '../../../application');
 
         } else {
             echo "$filename does not exist, cannot copy it to the application directory";
+        }
+    }
+
+
+    /**
+     * Recursive Copy
+     *
+     * @param string $src
+     * @param string $dst
+     */
+    private static function recursiveCopy($src, $dst)
+    {
+        mkdir($dst, 0755);
+
+        $iterator = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($src, \RecursiveDirectoryIterator::SKIP_DOTS),
+            \RecursiveIteratorIterator::SELF_FIRST
+        );
+
+        foreach ($iterator as $file) {
+            if ($file->isDir()) {
+                mkdir($dst . '/' . $iterator->getSubPathName());
+            } else {
+                copy($file, $dst . '/' . $iterator->getSubPathName());
+            }
         }
     }
 }
