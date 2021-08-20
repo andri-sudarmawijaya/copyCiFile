e064295d3b1434309c8a71f04aa20dbacb38626a
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2021-08-20 21:04:43 +0700

    update Installer

--- src/Installer.php
+++ src/Installer.php
@@ -11,10 +11,27 @@ class Installer {
         echo "hello, i am a page.";
     }
 
+    public static function postPackageInstall(PackageEvent $event = NULL)
+    {
+        $installedPackage = $event->getOperation()->getPackage();
+        // do stuff
+    }
+
+
+    public static function postInstall(Event $event = NULL)
+    {
+        $composer = $event->getComposer();
+        // do stuff
+
+        echo "hello, i am install a page.";
+    }
+
     public static function postUpdate(Event $event = NULL)
     {
         $composer = $event->getComposer();
         // do stuff
+
+        echo "hello, i am update a page.";
     }
 
     public static function postAutoloadDump(Event $event = NULL)
@@ -23,12 +40,6 @@ class Installer {
         require $vendorDir . '/autoload.php';
 
         //some_function_from_an_autoloaded_file();
-    }
-
-    public static function postPackageInstall(PackageEvent $event = NULL)
-    {
-        $installedPackage = $event->getOperation()->getPackage();
-        // do stuff
     }
 
     public static function warmCache(Event $event = NULL)
